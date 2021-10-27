a475aec27ce901c313c1db110265c45f6d536dd9
Opal <opal.mizrahi2@gmail.com>
2021-10-27 20:52:54 +0300

    conf

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int gappih    = 6;       /* horiz inner gap between window
 static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"FontAwesome:size=12", "Roboto:size=12" };
