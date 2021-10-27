31402674ff26058b0f9fee94c616059bf70ec91f
Opal <opal.mizrahi2@gmail.com>
2021-10-28 00:25:28 +0300

    configs

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"FontAwesome:size=12", "Roboto:size=12" };
@@ -35,8 +35,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Java",     NULL,       NULL,       0,            1,           -1 },
+	{ "Galculator",NULL,      NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -51,9 +51,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-// static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
-// static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
-// static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 static const Layout layouts[] = {
@@ -90,15 +90,27 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *librewolf[]  = { "librewolf", NULL };
+static const char *brave[]  = { "brave", NULL };
+static const char *applauncher[] = {"rofi", "-modi", "drun", "-show", "drun"};
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_b,      spawn,          {.v = {"librewolf"} } },
-  { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = {"librewolf"} } },
+  { MODKEY,                       XK_b,      spawn,          {.v = librewolf } },
+  { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = brave } },
+  { MODKEY,                       XK_r,      spawn,          SHCMD("alacritty -e ranger") },
+  { MODKEY,                       XK_d,      spawn,          SHCMD("pcmanfm") },
 
- //  { 0,                       PrintScreenDWM,      spawn,          {.v = brave } },
+
+  { Mod1Mask,                     XK_F1,      spawn,          {.v = applauncher } },
+  { 0,                            XK_Print,   spawn,          SHCMD("maim -s ~/Pictures/Screenshots/$(date +%s).png") },
+  { MODKEY,                       XK_Print,   spawn,          SHCMD("flameshot gui") },
+  { Mod1Mask,                     XK_v,       spawn,          SHCMD("roficlip") },
+  { Mod1Mask|ControlMask,         XK_x,       spawn,          SHCMD("xkill") },
+  { Mod1Mask|ControlMask|ShiftMask,XK_Delete, spawn,          SHCMD("htop") },
 	// { 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	// { 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
 	// { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
@@ -106,21 +118,23 @@ static Key keys[] = {
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_e,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
   { MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
 	{ MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
-  { MODKEY|ControlMask,           XK_h,      setcfact,       {.f = +0.25} },
-	{ MODKEY|ControlMask,           XK_l,      setcfact,       {.f = -0.25} },
+  { MODKEY|ControlMask,           XK_k,      setcfact,       {.f = +0.25} },
+	{ MODKEY|ControlMask,           XK_j,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ControlMask,           XK_o,      setcfact,       {.f =  0.00} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ControlMask,   	    	XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_s,      togglefloating, {0} },
 
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -146,7 +160,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[3]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
